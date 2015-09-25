c20e484fc481e7ab11dd8d5751133deee168863e
Sergey Nabiulin <sergei.nabiulin@gmail.com>
2015-09-25 19:24:39 +0300

    Added example

--- example/app.js
+++ example/app.js
@@ -3,15 +3,14 @@
 var app = angular.module('example', ['wavesurfer.angular']);
 
 app.controller('wavesurferController', ['$scope', function ($scope) {
-    $scope.options = {
-        waveColor      : '#c5c1be',
-        progressColor  : '#2A9FD6',
-        normalize      : true,
-        hideScrollbar  : true,
-        skipLength     : 15,
-        height         : 53,
-        cursorColor    : '#2A9FD6'
-    };
+//    $scope.options = {
+//        waveColor      : '#c5c1be',
+//        progressColor  : '#2A9FD6',
+//        hideScrollbar  : true,
+//        skipLength     : 15,
+//        height         : 53,
+//        cursorColor    : '#2A9FD6'
+//    };
     
     $scope.url = './example.mp3';
 }]);
